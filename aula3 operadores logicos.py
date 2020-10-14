c00cd3a579f57c1771aa30a8445fc388d9368001
João Flavo Costa Teixeira Filho <72360863+Joaoflavo@users.noreply.github.com>
2020-10-14 03:18:55 -0300

    Add files via upload
    Programa de treino feito para aprender a usar operadores lógicos desenvolvido em python
    Programa desenvolvido por: João Flavo J°@°2@2@ 14-10-2020 03:14 AM

---
+++ aula3 operadores logicos.py
@@ -0,0 +1,39 @@
+#programa de treino feito para aprender a usar operadores lógicos
+#programa desenvolvido por: João Flavo J°@°2@2@ 14-10-2020 03:14 AM
+print ('Escolha uma opção e descubra o que acontece')
+print('Menu:\n1 = Escolha Guilherme: \n2 = Escolha João:\n3 = Escolha Maria:')
+
+opcao = input('Escolha uma opção do menu:')
+
+if opcao == '1':
+    print('Guilherme é um anjo brother!')
+elif opcao == '2':
+    print('João vc é  muito louco brother!')
+elif opcao == '3':
+    print('Maria vc e ruim vai é pro inferno')
+else:
+    print('Vc não quer saber sobre ninguem, esta opção é invalida , Por favor, reinicie o programa!')
+
+
+
+
+
+
+
+# var_verdade = True # boolean ou boleano
+# var_false = False
+# if var_verdade == True:
+#     print('var verdade é verdadeiro')
+
+
+#print(type(var_verdade),type(var_false))
+
+# Comparações
+# and  significa e
+# or significa ou
+# == igual
+# !=diferente
+# < menor
+# > maior
+# <= menor igual
+# >= maior igual
